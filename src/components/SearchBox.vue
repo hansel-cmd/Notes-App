41e9f5cdf68dab30c40f2bcf693ea174bcee575b
hansel-cmd <hanselcesa@gmail.com>
2023-12-12 21:23:27 +0800

    - dynamically render search box input

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-b-[1px] p-2 h-[64px] relative">
+    <div v-if="showSearchBox" class="border-b-[1px] p-2 h-[64px] relative">
         <div className="flex items-center border-[1px] px-2 py-2 grow">
             <input type="text" @keyup.enter="handleSearch"
                 className="w-full ms-2 bg-transparent focus:outline-none dark:text-white border-0 border-transparent focus:ring-0" />
@@ -13,6 +13,16 @@
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+import { EDIT, CREATE } from '@/router/routes';
+import { computed } from 'vue';
+
+const route = useRoute();
+
+const showSearchBox = computed(() => {
+    const regex = new RegExp(`^${EDIT.replace('/:id', '/\\d+')}$`);
+    return route.path !== CREATE && !regex.test(route.path);
+})
 
 const handleSearch = (e) => {
     console.log(e.target.value)
